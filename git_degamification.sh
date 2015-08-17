ae465dd5897a39126b16c464ea8162598dda771e
Jeff Shrader <jeff20029@gmail.com>
2015-08-17 11:50:47 -0700

    Regular commit at 2015-08-17

--- git_degamification.sh
+++ git_degamification.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 bin_path="/Users/jgs/google_drive/bin"
 timestamp=$(date +%Y-%m-%d)
-file="$bin_path/degamification/growth.txt"
+file_path="$bin_path/degamification"
+file="$file_path/growth.txt"
 
 echo "A new line on $timestamp" > $file
-$bin_path/scripts/git_commit.sh $file
+$bin_path/scripts/git_commit.sh $file_path/path.txt
