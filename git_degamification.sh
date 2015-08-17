c3cc71d183808eb476b561a23adc68a6197a9d9b
Jeff Shrader <jeff20029@gmail.com>
2015-08-17 11:58:00 -0700

    minor

--- git_degamification.sh
+++ git_degamification.sh
@@ -4,5 +4,5 @@ timestamp=$(date +%Y-%m-%d)
 file_path="$bin_path/degamification"
 file="$file_path/growth.txt"
 
-echo "A new line on $timestamp" > $file
+echo "A new line on $timestamp" >> $file
 $bin_path/scripts/git_commit.sh $file_path/path.txt
