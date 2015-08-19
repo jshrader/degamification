828395d14735cc18b2f77084b0414b8a58abb533
Jeff Shrader <jeff20029@gmail.com>
2015-08-18 22:34:57 -0700

    Added commit.sh to main directory

---
+++ git_commit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# A bash script to automatically perform routine git commits.
+# To use this, run
+#  git_commit.sh < file_name
+# OR
+#  git_commit.sh < text_file_with_file_names
+while read line
+do
+    f=$(echo $line|tr -d '\n')
+    f=$HOME/$f
+    echo "Committing $f"
+    #touch $f/test.txt
+    cd $f    
+    git add *
+    timestamp=$(date +%Y-%m-%d)
+    echo "$timestamp"
+    git commit -m "Regular commit at $timestamp"
+done < $1
+
--- git_degamification.sh
+++ git_degamification.sh
@@ -5,4 +5,5 @@ file_path="$bin_path/degamification"
 file="$file_path/growth.txt"
 
 echo "A new line on $timestamp" >> $file
-$bin_path/scripts/git_commit.sh $file_path/path.txt
+$file_path/git_commit.sh $file_path/path.txt
+git push -u origin master
